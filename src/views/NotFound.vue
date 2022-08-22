08f3b8669f5f20122bffcb023d4a9a28434c77be
pengmingtao <pengmingtao2022@163.com>
2022-08-22 16:50:29 +0800

    添加NotFound页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div>输入的网址不存在，请重新输入</div>
+    <div><routerLink to="/money">返回首页</routerLink></div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
