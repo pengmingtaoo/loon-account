0ed2309805c7fae6d032aa585aa2f1b8352f10f7
pengmingtao <pengmingtao2022@163.com>
2022-08-27 10:23:41 +0800

    li无法选中取消变色

--- src/components/money/Tags.vue
+++ src/components/money/Tags.vue
@@ -1,43 +1,54 @@
 <template>
   <div class="tags">
-    <div class="new">
-      <span class="other"><Icon name="other"/>其他</span>
-    </div>
+    <!--    <div class="new">-->
+    <!--      <span class="other"><Icon name="other"/>其他</span>-->
+    <!--    </div>-->
     <ul class="current">
-      <li><Icon name="other" class="selected"/> 其他</li>
-      <li><Icon name="shop"/>购物</li>
-      <li><Icon name="canyin"/>餐饮</li>
-      <li><Icon name="house"/>住房</li>
-      <li><Icon name="bus"/> 交通</li>
-      <li><Icon name="clothes"/>服饰</li>
-      <li><Icon name="riyongpin"/>日用品</li>
-      <li><Icon name="recreation"/> 娱乐</li>
-      <li><Icon name="food_ingredient"/>食材</li>
-      <li><Icon name="junk_food"/>零</li>
-      <li><Icon name="yanjiucha"/>烟酒茶</li>
-      <li><Icon name="study"/>学习</li>
-      <li><Icon name="medical"/>医疗</li>
-      <li><Icon name="house"/>水电煤</li>
-      <li><Icon name="shuidianmei"/>交通</li>
-      <li><Icon name="recreation"/>通讯</li>
-      <li><Icon name="red_packet"/>人情往来</li>
+      <li v-for="tag in dataSource" :key="tag"
+          :class="{selected:selectedTags.includes(tag)==true}"
+          @click="select(tag)"><span>{{ tag }}</span></li>
+      <!--      <li><Icon name="other" class="selected"/> 其他</li>-->
+      <!--      <li><Icon name="shop"/>购物</li>-->
+      <!--      <li><Icon name="canyin"/>餐饮</li>-->
+      <!--      <li><Icon name="house"/>住房</li>-->
+      <!--      <li><Icon name="bus"/> 交通</li>-->
+      <!--      <li><Icon name="clothes"/>服饰</li>-->
+      <!--      <li><Icon name="riyongpin"/>日用品</li>-->
+      <!--      <li><Icon name="recreation"/> 娱乐</li>-->
+      <!--      <li><Icon name="food_ingredient"/>食材</li>-->
+      <!--      <li><Icon name="junk_food"/>零</li>-->
+      <!--      <li><Icon name="yanjiucha"/>烟酒茶</li>-->
+      <!--      <li><Icon name="study"/>学习</li>-->
+      <!--      <li><Icon name="medical"/>医疗</li>-->
+      <!--      <li><Icon name="house"/>水电煤</li>-->
+      <!--      <li><Icon name="shuidianmei"/>交通</li>-->
+      <!--      <li><Icon name="recreation"/>通讯</li>-->
+      <!--      <li><Icon name="red_packet"/>红包</li>-->
     </ul>
+    <div class="add">
+      <button>新增标签</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Prop} from 'vue-property-decorator';
 
 @Component
 export default class Tags extends Vue {
+  @Prop(Array) dataSource: string[] | undefined;
+  selectedTags: string[] = [];
 
-
-  newTag = 'other';
-  handle(tag:string){
-    this.newTag = tag;
+  select(tag: string) {
+   const index = this.selectedTags.push(tag);
+   if(this.selectedTags.includes(tag)==true){
+     this.selectedTags.splice(index,1);
+   }else {
+     this.selectedTags.push(tag);
+     console.log(this);
+   }
   }
-
 }
 
 </script>
@@ -46,7 +57,10 @@ export default class Tags extends Vue {
 @import "~@/assets/style/helper.scss";
 
 .tags {
-  $c2: #F0625A;
+  $fc1: #BABABA;
+  $fc2: #fff;
+  $bc1: #f6f6f6;
+  $bc2: #F0625A;
   font-size: 16px;
   display: flex;
   flex-direction: column;
@@ -59,58 +73,77 @@ export default class Tags extends Vue {
     flex-grow: 1;
 
     > li {
-      $h: 50px;
-      $c1: #BABABA;
+      $h: 70px;
       width: 20%;
       height: $h;
       display: flex;
       justify-content: center;
       align-items: center;
       flex-direction: column;
-      font-size: 10px;
-      color: $c1;
+      font-size: 12px;
+      color: $fc1;
+      background: $bc1;
+      padding: 10px;
+      border-radius: 50%;
 
-      .icon {
-        width: 24px;
-        height: 24px;
-        margin-bottom: 4px;
+      //.icon {
+      //  width: 44px;
+      //  height: 44px;
+      //  margin-bottom: 4px;
+      //  padding: 4px;
+      //  border-radius: 50%;
+      //  background: #F6F6F6;
+      //}
+      span {
+
       }
 
-      .selected {
-        color: $c2;
+      &.selected {
+        background: $bc2;
+        color: $fc2;
       }
-
     }
+
   }
 
-  > .new {
-    padding-left: 32px;
-    display: flex;
-    align-items: center;
-    border-bottom: .2px solid rgb(8, 8, 8, 0.3);
-
-    .other {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: 14px;
-      .icon {
-        width: 30px;
-        height: 30px;
-        color: $c2;
-        margin-right:.5em;
-      }
-    }
-
+  .add {
+    font-size: 14px;
+    margin-left: 1em;
 
     button {
-      background: transparent;
       border: none;
-      color: #999;
-      border-bottom: 1px solid;
-      padding: 0 3px;
+      background: inherit;
     }
   }
+
+  //> .new {
+  //  padding-left: 32px;
+  //  display: flex;
+  //  align-items: center;
+  //  border-bottom: .2px solid rgb(8, 8, 8, 0.3);
+  //
+  //  .other {
+  //    display: flex;
+  //    justify-content: center;
+  //    align-items: center;
+  //    font-size: 14px;
+  //    .icon {
+  //      width: 30px;
+  //      height: 30px;
+  //      color: $c2;
+  //      margin-right:.5em;
+  //    }
+  //  }
+
+
+  //  button {
+  //    background: transparent;
+  //    border: none;
+  //    color: #999;
+  //    border-bottom: 1px solid;
+  //    padding: 0 3px;
+  //  }
+  //}
 
 }
 </style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,26 +1,32 @@
 <template>
   <layout classPrefix="layout" class="layout">
-  <Number-pad/>
-  <Tags/>
-  <Types/>
+    <Number-pad/>
+    <Tags :data-source="tags"/>
+    <Types/>
   </layout>
 </template>
 
-<script lang="ts">
+<script lang="js">
 
 import NumberPad from '@/components/money/NumberPad.vue';
 import Tags from '@/components/money/Tags.vue';
 import Types from '@/components/money/Types.vue';
+
 export default {
   name: 'Money',
   components: {Types, Tags, NumberPad},
+  data(){
+    return{
+      tags:['其他','服饰','餐饮','住房','交通'],
+    }
+  }
 
 };
 </script>
 
 <style lang="scss" scoped>
 
-.layout{
+.layout {
   min-width: 400px;
 
 }
