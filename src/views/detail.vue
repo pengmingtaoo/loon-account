3529676db184e1750c33be0e389460e994cf47a7
pengmingtao <pengmingtao2022@163.com>
2022-09-14 14:57:06 +0800

    detali.vue 使用Vuex

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -19,18 +19,26 @@ import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
 
 
-
 @Component({
-  components:{Button}
+  components:{Button},
+  computed:{
+    tags(){
+     return  this.$store.state.tagList;
+    }
+  },
 })
 export default class Detail extends Vue {
-  // tags = store.tagList;
-  tags=[];
+
+  beforeCreate(){
+    this.$store.commit('fetchTags');
+  }
+
   createTag(){
     const name  = window.prompt('请输入标签名！');
-    if(name){
-      // store.createTag(name);
+    if (!name) {
+      return window.alert('标签名不能为空！');
     }
+    this.$store.commit('createTag',name);
   }
 
 }
